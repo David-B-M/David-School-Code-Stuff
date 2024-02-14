967046cbd252e7f2c9aab4e15e5cd6af1a9eb467
David-B-M <144824116+David-B-M@users.noreply.github.com>
2024-02-13 23:24:14 -0500

    Add files via upload

--- About Me Webpage/homeMenu.js
+++ About Me Webpage/homeMenu.js
@@ -7,8 +7,20 @@ class HomeMenu extends React.Component{
        return(
         <div class="container-fluid">
 
-            <div class="row m-5">
-                <div class="col m-5"></div>
+            <div class="row m-3">
+                <div class="col-3"></div>
+                <div class="col-6 text-center title">
+                    <h1>
+                        David's Virtual Portfolio
+                    </h1>
+                </div>
+                <div class="col-3"></div>
+            </div>
+
+            <div class="row m-3">
+                <div class="col m-4">
+                    <img src="profile.jpg" width="350" height="350" class="imgStuff"></img>
+                </div>
                 <div class="col m-5 bg-info">
                     <h3 class="text-dark">
                         Welcome to this simple webpage highlighting me. Feel free to look around.
@@ -35,8 +47,10 @@ class AboutMe extends React.Component{
         <div class="container-fluid">
 
             <div class="row m-4">
-                <div class="col m-4"></div>
-                <div class="col m-4 bg-info">
+                <div class="col m-4">
+                    <img src="bonsai.jpg" width="300" height="400" class="imgStuff"></img>
+                </div>
+                <div class="col m-3 bg-info">
                     <h5 class="text-dark">
                         Hi there, my name is David Middour. 
                         I am a Senior at UMBC studying for a degree in Computer Science: Data Science.
@@ -55,7 +69,7 @@ class AboutMe extends React.Component{
                         <br></br>
                         Email: david.middour22@gmail.com
                         <br></br>
-                        Github:
+                        Github: TBD
                     </h5>
                 </div>
             </div>
@@ -107,13 +121,13 @@ class Projects extends React.Component{
                 <div class="col-4">
                     <div class="tab-content" id="nav-tabContent">
                     <div class="tab-pane fade show active bg-info" id="overview" role="tabpanel" aria-labelledby="list-home-list">
-                        <h5>
+                        <h4>
                         Welcome to the projects page. 
                         Here I will give an overview of different projects I have worked on.
-                        </h5>
+                        </h4>
                     </div>
                     <div class="tab-pane fade bg-info" id="smart-hub" role="tabpanel" aria-labelledby="list-profile-list">
-                        <h5>
+                        <h4>
                             The Smart-Home-Hub was a group project for my CMSC 437 Graphics User Interface course.
                             The project was to create a graphics user interface for a home system that could control
                             various settings like home temperature, fridge temperature, garden irrigation and more.
@@ -121,27 +135,28 @@ class Projects extends React.Component{
                             The project utilized HTML, CSS and JavaScript. 
                             Using the React framework in JavaScript and bootstrap, 
                             we were able to add lots of functionality and make the user interafce well designed.
-                        </h5>
+                        </h4>
                     </div>
                     <div class="tab-pane fade bg-info" id="spteam" role="tabpanel" aria-labelledby="list-messages-list">
-                        <h5>
+                        <h4>
                             SPteam was my first big group coding project at UMBC in my COMP 101Y course during my first semester.
                             In this introductory course to programming and computer science. 
                             In the course, we used a Java based tool called Processing which focused on visual elements, like being able to draw to the screen.
                             In the semester group project, me and my groupmates made a pac-man like game where the user could collect powerups and play through 15 levels of increasing difficulty.
-                        </h5>
+                        </h4>
                     </div>
                     <div class="tab-pane fade bg-info" id="cat-thing" role="tabpanel" aria-labelledby="list-settings-list">
-                        <h5>
+                        <h4>
                             Cat Thing is a personal project of mine where I am trying to create a desktop pet that simply exists on the desktop as you work or do whatever.
                             It uses a simple ai to make random choices about its movement, and then moves across the screen accordingly.
-                        </h5>
+                        </h4>
                     </div>
                     </div>
                 </div>
                 <div class="col-5">
                     <div class="tab-content" id="nav-tabContent">
                         <div class="tab-pane fade show active" id="overview-img" role="tabpanel" aria-labelledby="list-profile-list">
+                            <img src="cat-coding.png" width="400" height="400"></img>
                         </div>
                         <div class="tab-pane fade" id="smart-hub-img" role="tabpanel" aria-labelledby="list-profile-list">
                             <img src="Smart-Home-Hub.jpg" width="450" height="300"></img>
